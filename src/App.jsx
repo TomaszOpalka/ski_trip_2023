fa9ca33714d99262c03afe347b9f06930073dec9
TomaszOpalka <to31144@gmail.com>
2023-12-12 03:27:06 +0100

    cały projekt

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,145 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [count, setCount] = useState(0);
+  // Stan komponentu
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+  const [error, setError] = useState('');
+
+  // Efekt używany do odczytu komentarzy z localStorage po załadowaniu komponentu
+  useEffect(() => {
+    const storedComments = localStorage.getItem('comments');
+    if (storedComments) {
+      setComments(JSON.parse(storedComments));
+    }
+  }, []);
+
+  // Funkcja obsługująca dodawanie nowego komentarza
+  const addComment = () => {
+    if (name.trim() !== '' && comment.trim() !== '') {
+      const newComment = { name, comment, id: Date.now() };
+      setComments((prevComments) => [...prevComments, newComment]);
+      setName('');
+      setComment('');
+      setError('');
+    } else {
+      setError('Imię w komentarzu wymagane');
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('comments', JSON.stringify(comments));
+  }, [comments]);
   return (
     <>
+      <div className='ski_container'>
+        <div className='header'>
+          <h1>Wyjazd na Narty 26-30.12.2023</h1>
+        </div>
+        <div className='space' style={{ position: 'relative' }}>
+  <button className='space-btn'
+    onClick={() => {
+      window.scrollTo({
+        top: window.scrollY + window.innerHeight,
+        behavior: 'smooth',
+      });
+    }}
+  >
+   press
+  </button>
+</div>
+        <div className='hero'>
+           <h1>Kłodzko 2023</h1>
+           <ul>skład:
+            <li>Włodarz Krzystof Sankowski</li>
+            <li>nażeczona włodarza Olivia</li>
+            <li>Paulina</li>
+            <li>Tomek</li>
+            <li>Szymon Opałka</li>
+            <li>Kurasz</li>
+            <li>Filip przypalone jajo</li>
+            <li>dj Broniu</li>
+           </ul>
+        </div>
+        <div className='space' style={{ position: 'relative' }}>
+  <button className='space-btn'
+    
+    onClick={() => {
+      window.scrollTo({
+        top: window.scrollY + window.innerHeight,
+        behavior: 'smooth',
+      });
+    }}
+  >
+    &#8595;
+  </button>
+</div>
+        <div className='plan'>
+          <div className='plan-small'>
+          <h1>Plan</h1>
+          <p><span>26.12</span> wieczór przyjazd do Kłodzka na 2 auta Lexus(Szymon) Ford(Kuba)</p>
+          <p><span>27.12</span> rankiem atak na czarną górę</p>
+          <p><span>28.12</span> rano atak na Czeską górkę <br/>
+          wieczorem chlańsko</p>
+          <p><span>29.12</span> filip,Szymon powrót <br/>
+          Edwin przyjazd<br/>
+          Atak nocna Czarna góra<br/>
+          przyjazd mamy Krzysia takżę <br/>
+          <span>kulturka</span></p>
+          <p><span>30.12</span> Zawijka do Wro</p>
+        </div>
+        </div>
+        <div className='space' style={{ position: 'relative' }}>
+  <button className='space-btn'
+    style={{
+      position: 'absolute',
+      bottom: 0,
+      left: '50%',
+      transform: 'translateX(-50%)',
+      cursor: 'pointer',
+    }}
+    onClick={() => {
+      window.scrollTo({
+        top: window.scrollY + window.innerHeight,
+        behavior: 'smooth',
+      });
+    }}
+  >
+    &#8595;
+  </button>
+</div>
+        <div className='comments'>
+        <div>
+      <h2>Komentarz</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>
+          <input className='com-input' placeholder='imię' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <label>
+          <input className='com-input' placeholder='komentarz' type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+        </label>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={addComment}>Add Comment</button>
+      <div>
+        <h3>Comments:</h3>
+        <ul>
+          {comments.map((c) => (
+            <li className='com-li' key={c.id}>
+             <p> <strong>{c.name}:</strong> {c.comment}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+        </div>
+      </div>
     </>
   )
 }
